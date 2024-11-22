892a904ea9c2234d965abe8fe03df7d35c6428f4
Gaston <gastoncapdevila@hotmail.com>
2024-11-22 00:10:00 -0300

    menu burger terminado

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,15 +1,14 @@
 import "./header.css";
 import logo from "../../assets/img/caffe-logo-white.png";
-import { useContext } from "react";
-import { MenuContext } from "../../menuContext";
 import Menu from "../menu/Menu";
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { filterByFeature } from "../../slices/filterSlice";
 import { selectTotalItems } from "../../slices/cartSlice";
+import { useState } from "react";
 
 const Header = () => {
-  const { toggleMenu } = useContext(MenuContext);
+  const [isOpen, setIsOpen] = useState(false);
   const dispatch = useDispatch();
 
   const totalItems = useSelector(selectTotalItems);
@@ -19,13 +18,21 @@ const Header = () => {
       <Link to="/" onClick={() => dispatch(filterByFeature())}>
         <img src={logo} alt="prueba" className="logo-caffe" />
       </Link>
-      <Menu />
+      <Menu isOpen={isOpen} setIsOpen={setIsOpen} />
       <div className="menu-tools">
-        <a onClick={toggleMenu}>
-          <i className="fa-sharp fa-solid fa-bars menu-icon"></i>
-        </a>
+        {/* <a onClick={toggleMenu}> */}
+        <div
+          className={`menu-toggle ${isOpen && "open"}`}
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          {/* <i className="fa-sharp fa-solid fa-bars menu-icon"></i> */}
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
         <Link to="/cart">
           <i className="fa-sharp fa-solid fa-cart-shopping cart-icon"></i>
+
           <div className="counter-cart">{totalItems}</div>
         </Link>
         {/* <Link to="/">
--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -3,28 +3,27 @@ import React, { useContext } from "react";
 import { MenuContext } from "../../menuContext";
 import { Link } from "react-router-dom";
 
-const Menu = () => {
+const Menu = ({ isOpen, setIsOpen }) => {
   const { menu } = useContext(MenuContext);
 
   return (
-    <div className="search_container">
-      <nav className={`navbar_menu ${menu ? "isActive" : ""}`}>
-        <ul className="navbar_list">
-          <li>
-            <Link to="/">Destacados</Link>
-          </li>
-          <li>
-            <Link to="/about">Nosotros</Link>
-          </li>
-          <li>
-            <Link to="/products">Productos</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contacto</Link>
-          </li>
-        </ul>
-      </nav>
-    </div>
+    <nav className={`navbar-menu ${isOpen && "open"}`}>
+      <Link to="/" className="navbar-list-item">
+        Destacados
+      </Link>
+
+      <Link to="/about" className="navbar-list-item">
+        Nosotros
+      </Link>
+
+      <Link to="/products" className="navbar-list-item">
+        Productos
+      </Link>
+
+      <Link to="/contact" className="navbar-list-item">
+        Contacto
+      </Link>
+    </nav>
   );
 };
 
